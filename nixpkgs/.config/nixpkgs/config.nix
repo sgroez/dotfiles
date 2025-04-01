f14a1bee967f81ae70f7f3fcaca991fb7c54302e
venticx <venticx@gmail.com>
2025-04-01 11:05:38 +0200

    added all packages into bundles to have central config for all nix packages installed

--- nixpkgs/.config/nixpkgs/config.nix
+++ nixpkgs/.config/nixpkgs/config.nix
@@ -1,5 +1,7 @@
 {
+  allowUnfree = true;
   packageOverrides = pkgs: with pkgs; {
+    pianoteq = pkgs.callPackage /home/sim/Github/customNixPackages/pianoteq/default.nix { };
     systemPackages = pkgs.buildEnv {
       name = "system-packages";
       paths = [
@@ -8,7 +10,37 @@
 	gh
 	stow
 	neovim
+	lynx
+      ];
+    };
+    devPackages = pkgs.buildEnv {
+      name = "development-packages";
+      paths = [
+        docker
+	docker-compose
+	go
+      ];
+    };
+    fontPackages = pkgs.buildEnv {
+      name = "font-packages";
+      paths = [
 	jetbrains-mono
+      ];
+    };
+    guiPackages = pkgs.buildEnv {
+      name = "gui-packages";
+      paths = [
+	google-chrome
+	fuzzel
+	waybar
+	pavucontrol
+	brightnessctl
+	vscode
+	gimp
+	blender
+	postman
+	audacity
+	pianoteq
       ];
     };
   };
