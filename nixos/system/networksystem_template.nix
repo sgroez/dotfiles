e0ede230c90b33a36f87513453cf926badc33525
venticx <venticx@gmail.com>
2025-08-27 13:45:16 +0200

    removed unused nixos config from this repo

--- nixos/system/networksystem_template.nix
+++
@@ -1,8 +0,0 @@
-{
-  networking.hostName = "hostname";
-  networking.networkmanager.enable = true;
-  networking.firewall.enable = true;
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-}
--- nixos/userconfiguration_template.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }: {
-  users.users.username = {
-    isNormalUser = true;
-    description = "";
-    extraGroups = [ "wheel" ]; #add groups like networkmanager or docker
-    packages = with pkgs; [];
-  };
-}
