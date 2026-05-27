e21c4629f10029c97dc4c3d9707d6c3e253052e1
sgroez <62483704+sgroez@users.noreply.github.com>
2026-05-27 13:42:52 +0200

    fixed typos and syntax issues aswell as wrong config options

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -4,7 +4,7 @@
 
 hl.monitor({
     output = "",
-    mode = "highres@60".
+    mode = "preferred",
     position = "0x0",
     scale = 1,
 })
@@ -16,18 +16,6 @@ hl.monitor({
 
 local terminal = "kitty"
 local appLauncher = "fuzzel"
-
-
----------------------
----- ENV STYLES -----
----------------------
-
-local tabSize = 4
-local colorMain = "white"
-local colorAccent1 = "33ccff"
-local colorAccent2 = "00ff99"
-local colorInactive = "595959"
-local colorBackground = "000000"
 
 
 -------------------
@@ -51,8 +39,8 @@ hl.config({
         border_size = 2,
 
         col = {
-            active_border = { colors = {"rgb($colorAccent1)", "rgb($colorAccent2)"}, angle = 45 },
-            inactive_border = "rgb($colorInactive)",
+            active_border = { colors = {"rgb(33ccff)", "rgb(00ff99)"}, angle = 45 },
+            inactive_border = "rgb(595959)",
         },
     
 
@@ -62,28 +50,28 @@ hl.config({
     misc = {
         force_default_wallpaper = -1,
         disable_hyprland_logo = false,
-        background_color = "0xB2BCAA",
+        background_color = "rgb(B2BCAA)",
     },
 
     decoration = {
         active_opacity = 1.0,
         inactive_opacity = 1.0,
 
-        blur {
+        blur = {
             enabled = false,
         },
 
-        shadow {
+        shadow = {
             enabled = false,
         },
     },
 
-    animations {
+    animations = {
         enabled = false,
     },
 
-    master {
-        new_status = slave,
+    master = {
+        new_status = "slave",
     },
 })
 
@@ -93,7 +81,7 @@ hl.config({
 ---------------
 
 hl.config({
-    input {
+    input = {
         kb_layout = "de",
         kb_variant = "",
         kb_model = "",
@@ -112,7 +100,7 @@ hl.config({
 
 local mainMod = "ALT"
 
-hl.bind(mainMod .. " + SPACE", hl.dsp.exec_cmd(app_launcher))
+hl.bind(mainMod .. " + SPACE", hl.dsp.exec_cmd(appLauncher))
 hl.bind(mainMod .. " + C", hl.dsp.window.close())
 hl.bind(mainMod .. " + ESCAPE", hl.dsp.exec_cmd("shutdown -h now"))
 hl.bind(mainMod .. " + RETURN", hl.dsp.exec_cmd(terminal))
@@ -124,10 +112,10 @@ hl.bind(mainMod .. " + K", hl.dsp.focus({ direction = "up" }))
 hl.bind(mainMod .. " + J", hl.dsp.focus({ direction = "down" }))
 
 -- Move windows with mainMod + shift + vim move
-hl.bind(mainMod .. " + SHIFT + H", dsp.window.move({ direction = "left" }))
-hl.bind(mainMod .. " + SHIFT + L", dsp.window.move({ direction = "right" }))
-hl.bind(mainMod .. " + SHIFT + K", dsp.window.move({ direction = "up" }))
-hl.bind(mainMod .. " + SHIFT + J", dsp.window.move({ direction = "down" }))
+hl.bind(mainMod .. " + SHIFT + H", hl.dsp.window.move({ direction = "left" }))
+hl.bind(mainMod .. " + SHIFT + L", hl.dsp.window.move({ direction = "right" }))
+hl.bind(mainMod .. " + SHIFT + K", hl.dsp.window.move({ direction = "up" }))
+hl.bind(mainMod .. " + SHIFT + J", hl.dsp.window.move({ direction = "down" }))
 
 -- Switch workspaces with mainMod + [0-9]
 -- Move window to workspace with mainMod + shift + [0-9]
@@ -142,4 +130,4 @@ hl.bind(mainMod .. " + S", hl.dsp.exec_cmd("hyprshot -m region --clipboard-only"
 
 -- Brightness controll
 hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl set 10%+"))
-bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl set 10%-"))
+hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl set 10%-"))
